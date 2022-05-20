2d414d06d79066016dfec42092a327793660f94f
sovisrushain <sovisrushain@gmail.com>
2022-05-20 14:32:11 +0530

    add logs - student controller

--- src/main/java/com/cisco/resttemp/controller/StudentController.java
+++ src/main/java/com/cisco/resttemp/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.cisco.resttemp.controller;
 
 import com.cisco.resttemp.dto.StudentDTO;
 import com.cisco.resttemp.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +13,38 @@ import java.util.List;
 @RequestMapping("api/v1/students")
 public class StudentController {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     private StudentService studentService;
 
     @PostMapping
     public Integer saveStudent(@RequestBody StudentDTO studentDTO){
+        logger.info("student controller - post student: {}", studentDTO);
         return studentService.saveStudent(studentDTO);
     }
 
     @GetMapping("/{id}")
     public StudentDTO getStudentById(@PathVariable String id){
+        logger.info("student controller - get Student: {}", id);
         return studentService.getStudentById(id);
     }
 
     @GetMapping
     public List<StudentDTO> getAllStudents(){
+        logger.info("student controller - get all students");
         return studentService.getAllStudents();
     }
 
     @PutMapping("/{id}")
     public Integer updateStudent(@RequestBody StudentDTO studentDTO){
+        logger.info("student controller - update student: {}", studentDTO);
         return studentService.updateStudent(studentDTO);
     }
 
     @DeleteMapping("/{id}")
     public Integer deleteStudent(@PathVariable String id){
+        logger.info("student controller - delete student: {}", id);
         return studentService.deleteStudent(id);
     }
 
